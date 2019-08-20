cac41b6fbb7b5c6de41f53dc4262072db16bf48d
M4cs <mabridgland@protonmail.com>
2019-08-20 14:19:12 -0700

    Push for 1.0

---
+++ domoji/lib.py
@@ -0,0 +1,60 @@
+import requests
+import json
+import sys
+from terminaltables import AsciiTable
+from crayons import *
+
+def get_emoji_domain(query):
+    headers = {
+        'Content-Type': 'application/json'
+    }
+    response = requests.get('https://xn--i-7iq.ws/emojidomain/{}?format=json'.format(query), headers=headers)
+    return response.json()
+
+def convert_json_to_table(response):
+    table_data = [
+        ['Available', 'ASCII Domain', 'Price']
+    ]
+    ascii_domain = response['theAsciiDomain']
+    if response['available'] == False:
+        table_data.append([
+            str(red('False', bold=True)), ascii_domain, 'Check GoDaddy.com'
+        ])
+        table = AsciiTable(table_data, 'Results')
+        return table.table
+    else:
+        for result in response['results']:
+            if result['available']:
+                available = green('True', bold=True)
+            else:
+                available = red('False', bold=True)
+            table_data.append([
+                str(available), result['domain'], result.get('price') if result.get('price') is not None else 'null'
+            ])
+        table = AsciiTable(table_data, 'Results')
+        return table.table
+
+def menu():
+    sys.stdout.flush()
+    print(blue('\nDomoji v1\nCreated by @maxbridgland\n', bold=True))
+    print(white('Easily find emoji domains from the command line. Uses i❤️.ws API for domains.', bold=True))
+    print(white('Domains may be available for other tLDs like .com, .org, .co, etc. Check GoDaddy.com for full results.', bold=True))
+    while True:
+        cont = True
+        print(green('\nPlease enter the domain with enojis you\'d like:', bold=True))
+        query = input('\n~ ')
+        print(yellow('\nSearching for available domains with: {}'.format(query), bold=True))
+        response = get_emoji_domain(query)
+        print(convert_json_to_table(response))
+        while True:
+            print('\nWould you like to search for another domain? [Y\\n]')
+            answer = input('\n~ ').lower()
+            if answer == 'y':
+                break
+            elif answer == 'n':
+                cont = False
+                break
+            else:
+                print('\nUnknown Choice')
+        if not cont:
+            break
---
+++ setup.py
@@ -0,0 +1,40 @@
+import setuptools
+
+def readme():
+    try:
+        with open('README.md') as f:
+            return f.read()
+    except IOError:
+        return ''
+    
+setuptools.setup(
+    name="Domoji",
+    version="1.0",
+    author="Max Bridgland",
+    install_requires=[
+        'requests==2.21.0',
+        'terminaltables==3.1.0',
+        'crayons==0.2.0',
+        'colorama==0.4.1'
+    ],
+    author_email="mabridgland@protonmail.com",
+    description="Find Emoji Domains From The Command Line (Works on MacOS and Terminals That Accept Emojis",
+    long_description=readme(),
+    long_description_content_type="text/markdown",
+    entry_points = {
+        'console_scripts': [
+            'domoji = domoji.__main__:menu'
+        ]
+    },
+    keywords="domains, api, wrapper, emoji, emojicode, macOS",
+    url="https://github.com/M4cs/Domoji",
+    packages=setuptools.find_packages(),
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Topic :: Multimedia :: Graphics"
+    ),
+)
